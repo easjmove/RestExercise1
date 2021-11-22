af9147ce143e9693c8323ae7971771430a5e94ce
move <move@zealand.dk>
2021-11-22 12:10:52 +0100

    more comments

--- RestExercise1/Controllers/ItemsController.cs
+++ RestExercise1/Controllers/ItemsController.cs
@@ -14,36 +14,53 @@ namespace RestExercise1.Controllers
     [ApiController]
     public class ItemsController : ControllerBase
     {
+        //A object of the ItemsManager class
+        //Might be created several times, but the internal List is static
         private ItemsManager _manager = new ItemsManager();
-        // GET: api/<ItemsController>
+
+        // GET: api/Items
+        //Gets all the items in the managers list
         [HttpGet]
         public IEnumerable<Item> Get()
         {
             return _manager.GetAll();
         }
 
-        // GET api/<ItemsController>/5
+        // GET api/Items/5
+        //Gets a specific Item in the managers list, return null if the object wasn't found
+        //Notice the "{id}" part of the annotation, this makes the URI to the object expect a /int
         [HttpGet("{id}")]
         public Item Get(int id)
         {
             return _manager.GetById(id);
         }
 
-        // POST api/<ItemsController>
+        // POST api/Items
+        //Sends the new object to list, which gives it a new ID and return the newly created object
+        //Notice the FromBody part of the parameter, this expects a Json body from the request
         [HttpPost]
         public Item Post([FromBody] Item newItem)
         {
             return _manager.Add(newItem);
         }
 
-        // PUT api/<ItemsController>/5
+        // PUT api/Items/5
+        //Sends the id and the Item object to the manager to update the values
+        //The id in the object is ignored
+        //Returns null if no items has the id
+        //As seen here, we can mix different ways of expected data from the client
+        //Notice the "{id}" part of the annotation, this makes the URI to the object expect a /int
+        //Notice the FromBody part of the parameter, this expects a Json body from the request
         [HttpPut("{id}")]
         public Item Put(int id, [FromBody] Item updatedItem)
         {
             return _manager.Update(id, updatedItem);
         }
 
-        // DELETE api/<ItemsController>/5
+        // DELETE api/Items/5
+        //Asks the Manager to delete the item with the specific id
+        //Returns null if the item was not found
+        //Notice the "{id}" part of the annotation, this makes the URI to the object expect a /int
         [HttpDelete("{id}")]
         public Item Delete(int id)
         {
--- RestExercise1/Managers/ItemsManager.cs
+++ RestExercise1/Managers/ItemsManager.cs
@@ -8,7 +8,10 @@ namespace RestExercise1.Managers
 {
     public class ItemsManager
     {
+        //Keeps track of ids, in order for all items to have a unique ID
         private static int _nextId = 1;
+        //Creates the list of Items and fills it with 3 items to begin with
+        //The 3 items is only for testing purposes
         private static readonly List<Item> Data = new List<Item>
         {
             new Item {Id = _nextId++, Name = "Book about C#", ItemQuality = 300, Quantity = 10},
@@ -17,6 +20,7 @@ namespace RestExercise1.Managers
             // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
         };
 
+        //Returns all items in the List, in a new List
         public List<Item> GetAll()
         {
             return new List<Item>(Data);
@@ -24,11 +28,15 @@ namespace RestExercise1.Managers
             // Callers should no get a reference to the Data object, but rather get a copy
         }
 
+        //Returns a specific Item from the list
+        //return null if the id is not found
         public Item GetById(int id)
         {
             return Data.Find(item => item.Id == id);
         }
 
+        //Adds a new Item to the list, and assigns it the next id
+        //returns the newly added Item
         public Item Add(Item newItem)
         {
             newItem.Id = _nextId++;
@@ -36,6 +44,8 @@ namespace RestExercise1.Managers
             return newItem;
         }
 
+        //Deletes the Item from the list with the specific Id
+        //Returns null of no Item has the Id
         public Item Delete(int id)
         {
             Item item = Data.Find(item1 => item1.Id == id);
@@ -44,6 +54,9 @@ namespace RestExercise1.Managers
             return item;
         }
 
+        //Updates the values of the Item with the specific Id
+        //Returns null of no Item has the Id
+        //Notice Id is not changed in the Item from the List
         public Item Update(int id, Item updates)
         {
             Item item = Data.Find(item1 => item1.Id == id);
